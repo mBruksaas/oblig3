3610452638f92809d281f6330915187bf0ffe914
mBruksaas <martin_4144@hotmail.com>
2015-03-03 16:46:49 +0100

    v0.1

--- Oblig 3/src/Kort.java
+++ Oblig 3/src/Kort.java
@@ -1,4 +1,30 @@
 
-public class Kort {
-
+abstract class Kort {
+	private String forNavn;
+	private String etterNavn;
+	private int pin;
+	private int kortNr;
+	private int nesteKortNr = 1;
+	private boolean sperretKort;
+	
+	public Kort(String forNavn, String etterNavn, int pin) {
+		this.forNavn = forNavn;
+		this.etterNavn = etterNavn;
+		this.pin = pin;
+		kortNr = nesteKortNr;
+		nesteKortNr++;
+		sperretKort = false;
+	}
+	
+	public String getNavn() { return (forNavn + " " + etterNavn); }
+	
+	public Boolean isSperret() { return sperretKort; }
+	
+	public Boolean sjekkPin(int pin) { return (this.pin == pin); }
+	
+	public String toString() {
+		return ("Navn: " + forNavn + " " + etterNavn + 
+				"\nPin: " + pin + "\nKortnummer: " + kortNr + 
+				"\nKortet er " + (sperretKort ? "sperret" : "ikke sperret"));
+	}
 }
